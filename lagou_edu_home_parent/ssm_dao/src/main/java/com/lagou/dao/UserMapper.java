4807b4668ca453f6d202afb8a23c5ac7a9c4931c
lkjl-521 <70681967+lkjl-521@users.noreply.github.com>
2021-04-05 11:41:51 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -49,5 +49,7 @@ public interface UserMapper {
         获取用户拥有的资源信息
      */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
+    
+    public List<Resource> findResourceByRoleId2(List<Integer> ids);
 
 }
